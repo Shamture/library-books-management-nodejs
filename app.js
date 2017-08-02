80d1e3767ce25d5d5abc3c5482f678477d57b312
Marwen Doukh <marwen.doukh@gmail.com>
2017-08-02 11:47:57 +0100

    add email to signup api

--- app.js
+++ app.js
@@ -220,6 +220,7 @@ app.post('/person', function (req, res) {
     name: req.body.name,
     username: req.body.username,
     password: hash.sha512().update(req.body.password).digest('hex'),
+    email: req.body.email,
     type: req.body.type
 
   }).then(function (result) {
